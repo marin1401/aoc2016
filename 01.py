0b40bc2228d4fcd03be5ff6bb83fa8bce550f2b8
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-29 15:49:12 +0100

    Update 01.py

--- 01.py
+++ 01.py
@@ -3,65 +3,34 @@
 with open('./01.txt') as myinput:
     instructions = myinput.read().split(', ')
 
+direction = 1j
+location = 0 + 0j
+visited_locations = set()
+visited_locations.add(location)
+first_location_visited_twice = ''
+
+for instruction in instructions:
+    turn = instruction[0]
+    blocks = int(instruction[1:])
+    if turn == 'L':
+        direction *= 1j
+    elif turn == 'R':
+        direction *= -1j
+    for i in range(blocks):
+        location += direction
+        if first_location_visited_twice == '':
+            if location in visited_locations:
+                first_location_visited_twice = location
+            else:
+                visited_locations.add(location)
+
+def get_distance(location):
+    return int(abs(location.real) + abs(location.imag))
+
 #Part 1
 
-direction = 'N'
-x = 0
-y = 0
-path = [] #for part 2
-for instruction in instructions:
-    steps = int(instruction[1:])
-    if instruction[0] == 'L':
-        if direction == 'N':
-            for i in range(steps):
-                path.append((x-i, y))
-            x -= steps
-            direction = 'W'
-        elif direction == 'S':
-            for i in range(steps):
-                path.append((x+i, y))
-            x += steps
-            direction = 'E'
-        elif direction == 'W':
-            for i in range(steps):
-                path.append((x, y-i))
-            y -= steps
-            direction = 'S'
-        elif direction == 'E':
-            for i in range(steps):
-                path.append((x, y+i))
-            y += steps
-            direction = 'N'
-    else:
-        if direction == 'N':
-            for i in range(steps):
-                path.append((x+i, y))
-            x += steps
-            direction = 'E'
-        elif direction == 'S':
-            for i in range(steps):
-                path.append((x-i, y))
-            x -= steps
-            direction = 'W'
-        elif direction == 'W':
-            for i in range(steps):
-                path.append((x, y+i))
-            y += steps
-            direction = 'N'
-        elif direction == 'E':
-            for i in range(steps):
-                path.append((x, y-i))
-            y -= steps
-            direction = 'S'
-
-print(abs(x) + abs(y))
+print(get_distance(location))
 
 #Part 2
 
-path_set = set()
-for step_coords in path:
-    if step_coords in path_set:
-        print(abs(step_coords[0]) + abs(step_coords[1]))
-        break
-    else:
-        path_set.add(step_coords)
+print(get_distance(first_location_visited_twice))
