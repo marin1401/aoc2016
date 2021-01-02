bbbe6a92d754366ff1d7a0930ca51c929046ab1f
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-02 21:54:13 +0100

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,30 @@
+#Day 04
+
+with open('./04.txt') as myinput:
+    inputlines = myinput.readlines()
+
+rooms_list = [i.strip().replace('-', '') for i in inputlines]
+rooms = {i[:-10]: (int(i[-10:-7]), i[-6:-1]) for i in rooms_list}
+
+#Part 1
+
+id_sum = 0
+real_rooms = {}
+for k, v in rooms.items():
+    most_common = {char: k.count(char) for char in k}
+    sorted_most_common = sorted(most_common, key=lambda k: (-most_common[k], k))
+    if v[1] == ''.join(sorted_most_common[:5]):
+        id_sum += v[0]
+        real_rooms[k] = v
+
+print(id_sum)
+
+#Part 2
+
+for k, v in real_rooms.items():
+    decrypted = ''
+    for i in range(len(k)):
+        decrypted += chr((ord(k[i]) + v[0] - 97) % 26 + 97)
+    if 'northpole' in decrypted:
+        print(v[0])
+        break
