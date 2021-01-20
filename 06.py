2574cfd14bf2e8f53cf83a95ca0f276ee5024643
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-20 23:43:29 +0100

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,15 @@
+#Day 06
+
+with open('./06.txt') as myinput:
+    messages = myinput.readlines()
+
+all_letters = [[message.strip()[letter] for message in messages] for letter in range(len(messages[-1]))]
+letters_freq = [[(letters.count(letter), letter) for letter in set(letters)] for letters in all_letters]
+
+#Part 1
+
+print(''.join([max(letters)[1] for letters in letters_freq]))
+
+#Part 2
+
+print(''.join([min(letters)[1] for letters in letters_freq]))
