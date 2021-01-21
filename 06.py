389a6f4220a38633b3951bef35cf913f041ac9d3
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-21 12:50:15 +0100

    Update 06.py

--- 06.py
+++ 06.py
@@ -8,8 +8,8 @@ letters_freq = [[(letters.count(letter), letter) for letter in set(letters)] for
 
 #Part 1
 
-print(''.join([max(letters)[1] for letters in letters_freq]))
+print(''.join(max(letters)[1] for letters in letters_freq))
 
 #Part 2
 
-print(''.join([min(letters)[1] for letters in letters_freq]))
+print(''.join(min(letters)[1] for letters in letters_freq))
